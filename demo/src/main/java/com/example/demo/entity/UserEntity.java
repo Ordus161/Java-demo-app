1852ecc3e216a2b3776e8bff7bb14b3a1f18d434
Maxim Bondaruk <makc1992@list.ru>
2022-12-01 13:21:31 +0300

    User ToDos and models added

--- demo/src/main/java/com/example/demo/entity/UserEntity.java
+++ demo/src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,21 +1,19 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
-
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
     private String password;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<TodoEntity> todos;
     public UserEntity() {
 
     }
@@ -42,5 +40,13 @@ public class UserEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<TodoEntity> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<TodoEntity> todos) {
+        this.todos = todos;
     }
 }
